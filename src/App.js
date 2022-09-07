76733bdb1ec41061323c24f149ccb365b38aaf6f
Shay <shay2442@yahoo.com>
2022-09-07 13:34:14 -0500

    Made react to test different apis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [dogs, setDogs] = useState('')
+  const [image, setImage] = useState('https://dog.ceo/api/breeds/image/random')
+  const [likes, setLikes] = useState(0)
+
+  // useEffect(() => {
+  //   fetch ('https://dog.ceo/api/breeds/image/random')
+  // .then(r => r.json())
+  // .then((dogs) => setDogs(dogs))
+  // }, []); 
+
+  function handleNewDogClick() {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(r => r.json())
+    .then(({dog}) => { setDogs(dog.message)})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <img  src={dogs.message} height="150" width="150"  />
+     <button onClick={handleNewDogClick}>New Dog!</button>
     </div>
   );
 }
